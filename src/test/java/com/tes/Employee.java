0e7f5bbe5c66362ef9aa8f532c4c60242b8367e0
Balaji250692 <anigalanblue@gmail.com>
2021-02-13 15:42:18 -0500

    completed done

---
+++ src/test/java/com/tes/Employee.java
@@ -0,0 +1,23 @@
+package com.tes;
+
+public class Employee {
+	private void asd() {
+	System.out.println("a");
+
+	}
+	private void cde() {
+		System.out.println("b");
+
+	}
+	private void nvhd() {
+		System.out.println("c");
+
+	}
+	public static void main(String[] args) {
+		Employee e=new Employee();
+		e.asd();
+		e.cde();
+		e.nvhd();
+	}
+
+}
